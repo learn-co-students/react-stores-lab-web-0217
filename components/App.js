b6052d4b2e394d0d3116eef9d5265cdca6a11a33
gee2789 <geeyung.li@gmail.com>
2017-04-08 15:36:10 -0400

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,51 @@
 const React = require('react');
+const actions = require('../actions');
+const counterStore = require('../stores/counterStore');
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      // Your implementation here.
+      counter: counterStore.getState()
     };
+    this.incrementHandler = this.incrementHandler.bind(this)
+    this.decrementHandler = this.decrementHandler.bind(this)
   }
+
   componentDidMount () {
-    // Your implementation here.
+    this.removeListener = counterStore.addListener(counter => {
+      this.setState({ counter });
+    });
+    // counterStore.addListener((state) => {
+    //   this.setState(state)
+    // });
+    // this.setState(counterStore.getState())
   }
+
   componentWillUnmount () {
+    this.removeListener()  //how are we connecting this?
     // Your implementation here.
   }
+
+  incrementHandler(ev){
+    ev.preventDefault()
+    actions.increment()
+  }
+
+  decrementHandler(ev){
+    ev.preventDefault()
+    actions.decrement()  //where is action coming from?
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.decrementHandler}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.incrementHandler}>
             +
           </button>
         </div>
--- stores/Store.js
+++ stores/Store.js
@@ -1,4 +1,28 @@
 class Store {
+  constructor(initialState){
+    this.state = initialState
+    this.listeners = []
+  }
+
+  getState(){
+    return this.state
+  }
+
+  setState(state){
+    this.state = state
+    for (const listener of this.listeners) { //for ... of iterable statement.
+      listener.call(this, state)
+    }
+  }
+
+  addListener(listener){
+    this.listeners.push(listener)
+    const removeListener = () => {
+     this.listeners = this.listeners.filter((l) => listener !== l) //filters out the current listener defined on line 16
+    }
+   return removeListener; //why do we embed this function in addLister instead of it's independent thing?
+  }
+
   // Your implementation here.
   // Hint: Our <App /> component won't directly use this store, but instead the
   // CounterStore is going to inherit from it.
