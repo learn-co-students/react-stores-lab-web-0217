31b0a856af94a304c80c8ffa5018ad009c05255d
mwei2509 <m2dl89@gmail.com>
2017-04-10 14:06:28 -0400

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
+import CounterStore from '../stores/counterStore.js'
+import actions from '../actions'
+// const CounterStore = require '../stores/CounterStore.js'
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      counter: CounterStore.getState()
       // Your implementation here.
     };
+
   }
   componentDidMount () {
     // Your implementation here.
+    // add event listener to counterStore
+    this.removeListener = CounterStore.addListener((state)=>{
+      this.setState({counter: state})
+    })
+    this.setState(CounterStore.getState())
+
   }
   componentWillUnmount () {
     // Your implementation here.
+    //remove event listener from counterStore
+    this.removeListener()
   }
+
+  handleClick(action, event){
+    event.preventDefault()
+    action()
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button onClick={this.handleClick.bind(this, actions.decrement)} className='decrement'>
             -
           </button>
-          <button className='increment'>
+          <button onClick={this.handleClick.bind(this, actions.increment)} className='increment'>
             +
           </button>
         </div>
