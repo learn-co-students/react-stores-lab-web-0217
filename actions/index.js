da9306b06d309c49fbd70e27b38689bad21720fa
Alexandra Roth <alroth8@gmail.com>
2017-04-11 17:11:42 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,16 @@
-// Your implementation here.
-// Hint: We need two actions!
+import counterStore from '../stores/counterStore'
 
-export {}
+const increment = () => {
+	counterStore.increment()
+} 
+	
+
+
+const decrement = () => {
+	counterStore.decrement()
+}
+
+export default {
+	increment,
+	decrement
+}
